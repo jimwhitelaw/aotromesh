e13e3fc6011d1b351f4f40ba51dfe79b9300c3cd
Jim Whitelaw <jimwhitelaw@gmail.com>
2024-01-25 10:37:48 -0700

    Added flag module

--- src/modules/Modules.cpp
+++ src/modules/Modules.cpp
@@ -14,7 +14,8 @@
 #include "modules/NodeInfoModule.h"
 #include "modules/PositionModule.h"
 #include "modules/RemoteHardwareModule.h"
-#include "modules/ReplyModule.h"
+// #include "modules/ReplyModule.h"
+#include "modules/EFlagsModule.h"
 #include "modules/RoutingModule.h"
 #include "modules/TextMessageModule.h"
 #include "modules/TraceRouteModule.h"
@@ -53,91 +54,140 @@ void setupModules()
 {
         if (config.device.role != meshtastic_Config_DeviceConfig_Role_REPEATER)
         {
+                if (config.device.role != meshtastic_Config_DeviceConfig_Role_REPEATER)
+                {
 #if HAS_BUTTON || ARCH_PORTDUINO
-                inputBroker = new InputBroker();
+                        inputBroker = new InputBroker();
+                        inputBroker = new InputBroker();
 #endif
-                adminModule = new AdminModule();
-                nodeInfoModule = new NodeInfoModule();
-                positionModule = new PositionModule();
-                waypointModule = new WaypointModule();
-                textMessageModule = new TextMessageModule();
-                traceRouteModule = new TraceRouteModule();
-                neighborInfoModule = new NeighborInfoModule();
-                detectionSensorModule = new DetectionSensorModule();
-                atakPluginModule = new AtakPluginModule();
-                // Note: if the rest of meshtastic doesn't need to explicitly use your module, you do not need to assign the instance
-                // to a global variable.
+                        adminModule = new AdminModule();
+                        nodeInfoModule = new NodeInfoModule();
+                        positionModule = new PositionModule();
+                        waypointModule = new WaypointModule();
+                        textMessageModule = new TextMessageModule();
+                        traceRouteModule = new TraceRouteModule();
+                        neighborInfoModule = new NeighborInfoModule();
+                        detectionSensorModule = new DetectionSensorModule();
 
-                new RemoteHardwareModule();
-                new ReplyModule();
-                new EFlagsModule();
+                        // Note: if the rest of meshtastic doesn't need to explicitly use your module, you do not need to assign the instance
+                        // to a global variable.
+
+                        new RemoteHardwareModule();
+                        // new ReplyModule();
+                        new EFlagsModule();
 #if HAS_BUTTON || ARCH_PORTDUINO
-                rotaryEncoderInterruptImpl1 = new RotaryEncoderInterruptImpl1();
-                if (!rotaryEncoderInterruptImpl1->init())
-                {
-                        delete rotaryEncoderInterruptImpl1;
-                        rotaryEncoderInterruptImpl1 = nullptr;
-                }
-                upDownInterruptImpl1 = new UpDownInterruptImpl1();
-                if (!upDownInterruptImpl1->init())
-                {
-                        delete upDownInterruptImpl1;
-                        upDownInterruptImpl1 = nullptr;
-                }
-                cardKbI2cImpl = new CardKbI2cImpl();
-                cardKbI2cImpl->init();
+                        rotaryEncoderInterruptImpl1 = new RotaryEncoderInterruptImpl1();
+                        if (!rotaryEncoderInterruptImpl1->init())
+                        {
+                                delete rotaryEncoderInterruptImpl1;
+                                rotaryEncoderInterruptImpl1 = nullptr;
+                        }
+                        upDownInterruptImpl1 = new UpDownInterruptImpl1();
+                        if (!upDownInterruptImpl1->init())
+                        {
+                                delete upDownInterruptImpl1;
+                                upDownInterruptImpl1 = nullptr;
+                        }
+                        cardKbI2cImpl = new CardKbI2cImpl();
+                        cardKbI2cImpl->init();
+                        rotaryEncoderInterruptImpl1 = new RotaryEncoderInterruptImpl1();
+                        if (!rotaryEncoderInterruptImpl1->init())
+                        {
+                                delete rotaryEncoderInterruptImpl1;
+                                rotaryEncoderInterruptImpl1 = nullptr;
+                        }
+                        upDownInterruptImpl1 = new UpDownInterruptImpl1();
+                        if (!upDownInterruptImpl1->init())
+                        {
+                                delete upDownInterruptImpl1;
+                                upDownInterruptImpl1 = nullptr;
+                        }
+                        cardKbI2cImpl = new CardKbI2cImpl();
+                        cardKbI2cImpl->init();
 #ifdef INPUTBROKER_MATRIX_TYPE
-                kbMatrixImpl = new KbMatrixImpl();
-                kbMatrixImpl->init();
+                        kbMatrixImpl = new KbMatrixImpl();
+                        kbMatrixImpl->init();
+                        kbMatrixImpl = new KbMatrixImpl();
+                        kbMatrixImpl->init();
 #endif // INPUTBROKER_MATRIX_TYPE
 #endif // HAS_BUTTON
 #if ARCH_PORTDUINO
-                aLinuxInputImpl = new LinuxInputImpl();
-                aLinuxInputImpl->init();
+                        aLinuxInputImpl = new LinuxInputImpl();
+                        aLinuxInputImpl->init();
+                        aLinuxInputImpl = new LinuxInputImpl();
+                        aLinuxInputImpl->init();
 #endif
 #if HAS_TRACKBALL
-                trackballInterruptImpl1 = new TrackballInterruptImpl1();
-                trackballInterruptImpl1->init();
+                        trackballInterruptImpl1 = new TrackballInterruptImpl1();
+                        trackballInterruptImpl1->init();
+                        trackballInterruptImpl1 = new TrackballInterruptImpl1();
+                        trackballInterruptImpl1->init();
 #endif
 #if HAS_SCREEN
-                cannedMessageModule = new CannedMessageModule();
+                        cannedMessageModule = new CannedMessageModule();
+                        cannedMessageModule = new CannedMessageModule();
 #endif
 #if HAS_TELEMETRY
-                new DeviceTelemetryModule();
+                        new DeviceTelemetryModule();
+                        new DeviceTelemetryModule();
 #endif
 #if HAS_SENSOR
-                new EnvironmentTelemetryModule();
-                if (nodeTelemetrySensorsMap[meshtastic_TelemetrySensorType_PMSA003I].first > 0)
-                {
-                        new AirQualityTelemetryModule();
-                }
+                        new EnvironmentTelemetryModule();
+                        if (nodeTelemetrySensorsMap[meshtastic_TelemetrySensorType_PMSA003I].first > 0)
+                        {
+                                new AirQualityTelemetryModule();
+                        }
+                        new EnvironmentTelemetryModule();
+                        if (nodeTelemetrySensorsMap[meshtastic_TelemetrySensorType_PMSA003I].first > 0)
+                        {
+                                new AirQualityTelemetryModule();
+                        }
 #endif
 #if HAS_TELEMETRY && !defined(ARCH_PORTDUINO)
-                new PowerTelemetryModule();
+                        new PowerTelemetryModule();
+                        new PowerTelemetryModule();
 #endif
-#if (defined(ARCH_ESP32) || defined(ARCH_NRF52) || defined(ARCH_RP2040)) && !defined(CONFIG_IDF_TARGET_ESP32S2) && \
+#if (defined(ARCH_ESP32) || defined(ARCH_NRF52) || defined(ARCH_RP2040)) && !defined(CONFIG_IDF_TARGET_ESP32S2) &&     \
+    #if (defined(ARCH_ESP32) || defined(ARCH_NRF52) || defined(ARCH_RP2040)) && !defined(CONFIG_IDF_TARGET_ESP32S2) && \
     !defined(CONFIG_IDF_TARGET_ESP32C3)
-                new SerialModule();
+                        new SerialModule();
+                        new SerialModule();
 #endif
 #ifdef ARCH_ESP32
-                // Only run on an esp32 based device.
+                        // Only run on an esp32 based device.
+                        // Only run on an esp32 based device.
 #ifdef USE_SX1280
-                audioModule = new AudioModule();
+                        audioModule = new AudioModule();
+                        audioModule = new AudioModule();
 #endif
-                storeForwardModule = new StoreForwardModule();
-                paxcounterModule = new PaxcounterModule();
+                        storeForwardModule = new StoreForwardModule();
+                        paxcounterModule = new PaxcounterModule();
+                        storeForwardModule = new StoreForwardModule();
+                        paxcounterModule = new PaxcounterModule();
 #endif
 #if defined(ARCH_ESP32) || defined(ARCH_NRF52) || defined(ARCH_RP2040)
-                externalNotificationModule = new ExternalNotificationModule();
-                new RangeTestModule();
+                        externalNotificationModule = new ExternalNotificationModule();
+                        new RangeTestModule();
+                        externalNotificationModule = new ExternalNotificationModule();
+                        new RangeTestModule();
 #endif
-        }
-        else
-        {
-                adminModule = new AdminModule();
+                }
+                else
+                {
+                        adminModule = new AdminModule();
+                }
+                else
+                {
+                        adminModule = new AdminModule();
 #if HAS_TELEMETRY
-                new DeviceTelemetryModule();
+                        new DeviceTelemetryModule();
+                        new DeviceTelemetryModule();
 #endif
+                        traceRouteModule = new TraceRouteModule();
+                }
+                // NOTE! This module must be added LAST because it likes to check for replies from other modules and avoid sending extra
+                // acks
+                routingModule = new RoutingModule();
                 traceRouteModule = new TraceRouteModule();
         }
         // NOTE! This module must be added LAST because it likes to check for replies from other modules and avoid sending extra
