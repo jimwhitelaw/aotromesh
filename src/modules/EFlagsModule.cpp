b4aff2c048b898ad100af4625a3deb70dd436914
Jim Whitelaw <jimwhitelaw@gmail.com>
2024-01-26 13:34:58 -0700

    First pass with working code to change flag state Using Neopixel 7 led test board.

--- src/modules/EFlagsModule.cpp
+++ src/modules/EFlagsModule.cpp
@@ -2,25 +2,161 @@
 #include "MeshService.h"
 #include "configuration.h"
 #include "main.h"
+// #include <Adafruit_GFX.h>
+// #include <FastLED_NeoMatrix.h>
+// #include <FastLED.h>
+// #include <LEDMatrix.h>
 
-#include <assert.h>
+// Testing with NeoPixel 7 seg module
+#include <Adafruit_NeoPixel.h>
+#define PIN 4
 
-meshtastic_MeshPacket *EFlagsModule::allocReply()
+#define DELAY_INTERVAL 4000 // Send a flag change command every 4 sec
+#define BASE_STATION 1      // Flag stations only receive and respond to command. Only base unit sends commands
+
+EFlagsModule *eflagsModule;
+uint8_t flagState = 0;
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(7, PIN, NEO_RGB + NEO_KHZ800);
+
+// CRGB leds[NUMMATRIX];
+// // Define matrix width and height.
+// FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(leds, mw, mh,
+//                                                   NEO_MATRIX_TOP + NEO_MATRIX_RIGHT +
+//                                                       NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG);
+
+// void display_solid(uint16_t color)
+// {
+//     static uint16_t bmx, bmy;
+
+//     // Clear the space under the bitmap that will be drawn as
+//     // drawing a single color pixmap does not write over pixels
+//     // that are nul, and leaves the data that was underneath
+//     matrix->fillRect(bmx, bmy, bmx + 8, bmy + 8, LED_BLACK);
+//     matrix->drawBitmap(bmx, bmy, mono_bmp[2], 8, 8, color);
+//     bmx += 8;
+//     if (bmx >= mw)
+//         bmx = 0;
+//     if (!bmx)
+//         bmy += 8;
+//     if (bmy >= mh)
+//         bmy = 0;
+//     matrix->show();
+// }
+
+// void display_checkered()
+// {
+//     static uint16_t bmx, bmy;
+
+//     // Clear the space under the bitmap that will be drawn as
+//     // drawing a single color pixmap does not write over pixels
+//     // that are nul, and leaves the data that was underneath
+//     matrix->fillRect(bmx, bmy, bmx + 8, bmy + 8, LED_BLACK);
+//     matrix->drawBitmap(bmx, bmy, mono_bmp[0], 8, 8, LED_WHITE_MEDIUM);
+//     bmx += 8;
+//     if (bmx >= mw)
+//         bmx = 0;
+//     if (!bmx)
+//         bmy += 8;
+//     if (bmy >= mh)
+//         bmy = 0;
+//     matrix->show();
+// }
+
+ProcessMessage EFlagsModule::handleReceived(const meshtastic_MeshPacket &mp)
 {
-    assert(currentRequest); // should always be !NULL
-#ifdef DEBUG_PORT
-    auto req = *currentRequest;
-    auto &p = req.decoded;
-    // The incoming message is in p.payload
-    LOG_INFO("Received message from=0x%0x, id=%d, msg=%.*s\n", req.from, req.id, p.payload.size, p.payload.bytes);
-#endif
+    if (!BASE_STATION) // Base (non-Flag Station) will ignore messages TODO: change this later? so that stations send an ack when
+                       // they have changed state
+    {
+        auto &p = mp.decoded;
+        LOG_INFO("Received flags msg from=0x%0x, id=0x%x, msg=%.*s\n", mp.from, mp.id, p.payload.size, p.payload.bytes);
+        char *msg = new char[80];
+        sprintf(msg, "Received flags msg from=0x%0x, id=0x%x, msg=%.*s\n", mp.from, mp.id, p.payload.size, p.payload.bytes);
+        screen->print(msg);
+        uint16_t car_num = (p.payload.bytes[2] << 8) | p.payload.bytes[3];
+        setFlagState(p.payload.bytes[0], car_num);
+    }
+    return ProcessMessage::STOP;
+}
 
-    screen->print("Sending reply\n");
+int32_t EFlagsModule::runOnce()
+{
+    if (BASE_STATION) // base station - sends flag commands
+    {
+        NodeNum nodenum = 0x08ab5ecc;
+        uint16_t car_number = UINT16_MAX;
 
-    const char *replyStr = "Message Received";
-    auto reply = allocDataPacket();                 // Allocate a packet for sending
-    reply->decoded.payload.size = strlen(replyStr); // You must specify how many bytes are in the reply
-    memcpy(reply->decoded.payload.bytes, replyStr, reply->decoded.payload.size);
+        if (firstTime) // first time we run, do setup stuff
+        {              // for testing, we alternate between sending green flag and yellow flag
+            firstTime = false;
+            flagState = FLAG_NONE;
+            sendFlagCommand(nodenum, FLAG_NONE, UINT16_MAX);
+            LOG_INFO("Intializing Base Station");
+        } else // typical function
+        {
+            if (flagState == FLAG_NONE || flagState == YELLOW_FLAG) {
+                flagState = GREEN_FLAG;
+                sendFlagCommand(nodenum, GREEN_FLAG, car_number);
+            } else {
+                flagState = YELLOW_FLAG;
+                sendFlagCommand(nodenum, YELLOW_FLAG, car_number);
+            }
+        }
+        return DELAY_INTERVAL;
+    } else // flag station - init led
+    {
+        if (firstTime) {
+            strip.begin();
+            strip.setBrightness(30);
+            strip.show(); // Initialize all pixels to 'off'
+            setFlagState(FLAG_NONE, UINT16_MAX);
+            firstTime = false;
+        }
+        return 0;
+    }
+}
 
-    return reply;
+void EFlagsModule::setFlagState(uint8_t state, uint16_t car_num)
+{
+    switch (state) {
+    case FLAG_NONE:
+        strip.clear();
+        strip.show();
+        break;
+
+    case GREEN_FLAG:
+        strip.fill(LED_GREEN_MEDIUM, 0, 7);
+        strip.show();
+        break;
+    case YELLOW_FLAG:
+        strip.fill(LED_YELLOW_MEDIUM, 0, 7);
+        strip.show();
+        break;
+    default:
+        break;
+    }
+}
+void EFlagsModule::sendFlagCommand(NodeNum dest, uint8_t cmd, uint16_t car_num)
+{
+    meshtastic_MeshPacket *p = allocDataPacket();
+    if (p) {
+        p->to = dest;
+        // set flag state
+        p->decoded.payload.bytes[0] = cmd;
+        // car_num is is two bytes
+        p->decoded.payload.bytes[1] = car_num & 0xFF00;
+        p->decoded.payload.bytes[2] = car_num & 0xFF;
+        p->decoded.payload.size = 3;
+    } else {
+        p->decoded.payload.size = 1;
+    }
+    service.sendToMesh(p);
 }
--- src/modules/EFlagsModule.h
+++ src/modules/EFlagsModule.h
@@ -1,20 +1,122 @@
 #pragma once
 #include "SinglePortModule.h"
+#define BRIGHTNESS 16
 
-/**
- * A simple example module that just replies with "Message received" to any message it receives.
- */
-class EFlagsModule : public SinglePortModule
+// https://learn.adafruit.com/adafruit-neopixel-uberguide/neomatrix-library
+// MATRIX DECLARATION:
+// Parameter 1 = width of EACH NEOPIXEL MATRIX (not total display)
+// Parameter 2 = height of each matrix
+// Parameter 3 = number of matrices arranged horizontally
+// Parameter 4 = number of matrices arranged vertically
+// Parameter 5 = pin number (most are valid)
+// Parameter 6 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the FIRST MATRIX; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs WITHIN EACH MATRIX are
+//     arranged in horizontal rows or in vertical columns, respectively;
+//     pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns WITHIN
+//     EACH MATRIX proceed in the same order, or alternate lines reverse
+//     direction; pick one.
+//   NEO_TILE_TOP, NEO_TILE_BOTTOM, NEO_TILE_LEFT, NEO_TILE_RIGHT:
+//     Position of the FIRST MATRIX (tile) in the OVERALL DISPLAY; pick
+//     two, e.g. NEO_TILE_TOP + NEO_TILE_LEFT for the top-left corner.
+//   NEO_TILE_ROWS, NEO_TILE_COLUMNS: the matrices in the OVERALL DISPLAY
+//     are arranged in horizontal rows or in vertical columns, respectively;
+//     pick one or the other.
+//   NEO_TILE_PROGRESSIVE, NEO_TILE_ZIGZAG: the ROWS/COLUMS OF MATRICES
+//     (tiles) in the OVERALL DISPLAY proceed in the same order for every
+//     line, or alternate lines reverse direction; pick one.  When using
+//     zig-zag order, the orientation of the matrices in alternate rows
+//     will be rotated 180 degrees (this is normal -- simplifies wiring).
+//   See example below for these values in action.
+
+#define mw 16
+#define mh 16
+#define NUMMATRIX (mw * mh)
+
+#define LED_BLACK 0
+
+#define LED_RED_VERYLOW (3 << 11)
+#define LED_RED_LOW (7 << 11)
+#define LED_RED_MEDIUM (15 << 11)
+#define LED_RED_HIGH (31 << 11)
+
+#define LED_GREEN_VERYLOW (1 << 5)
+#define LED_GREEN_LOW (15 << 5)
+#define LED_GREEN_MEDIUM (31 << 5)
+#define LED_GREEN_HIGH (63 << 5)
+
+#define LED_BLUE_VERYLOW 3
+#define LED_BLUE_LOW 7
+#define LED_BLUE_MEDIUM 15
+#define LED_BLUE_HIGH 31
+
+#define LED_YELLOW_VERYLOW (LED_RED_VERYLOW + LED_GREEN_VERYLOW)
+#define LED_YELLOW_LOW (LED_RED_LOW + LED_GREEN_LOW)
+#define LED_YELLOW_MEDIUM (LED_RED_MEDIUM + LED_GREEN_MEDIUM)
+#define LED_YELLOW_HIGH (LED_RED_HIGH + LED_GREEN_HIGH)
+
+#define LED_PURPLE_VERYLOW (LED_RED_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_PURPLE_LOW (LED_RED_LOW + LED_BLUE_LOW)
+#define LED_PURPLE_MEDIUM (LED_RED_MEDIUM + LED_BLUE_MEDIUM)
+#define LED_PURPLE_HIGH (LED_RED_HIGH + LED_BLUE_HIGH)
+
+#define LED_CYAN_VERYLOW (LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_CYAN_LOW (LED_GREEN_LOW + LED_BLUE_LOW)
+#define LED_CYAN_MEDIUM (LED_GREEN_MEDIUM + LED_BLUE_MEDIUM)
+#define LED_CYAN_HIGH (LED_GREEN_HIGH + LED_BLUE_HIGH)
+
+#define LED_WHITE_VERYLOW (LED_RED_VERYLOW + LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_WHITE_LOW (LED_RED_LOW + LED_GREEN_LOW + LED_BLUE_LOW)
+#define LED_WHITE_MEDIUM (LED_RED_MEDIUM + LED_GREEN_MEDIUM + LED_BLUE_MEDIUM)
+#define LED_WHITE_HIGH (LED_RED_HIGH + LED_GREEN_HIGH + LED_BLUE_HIGH)
+
+static const uint8_t PROGMEM mono_bmp[][8] = {
+    {// 0: checkered 1
+     B10101010, B01010101, B10101010, B01010101, B10101010, B01010101, B10101010, B01010101},
+
+    {// 1: checkered 2
+     B01010101, B10101010, B01010101, B10101010, B01010101, B10101010, B01010101, B10101010},
+
+    {// 2: solid
+     B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111},
+};
+
+class EFlagsModule : public SinglePortModule, private concurrency::OSThread
 {
   public:
-    /** Constructor
-     * name is for debugging output
-     */
-    EFlagsModule() : SinglePortModule("eflags", meshtastic_PortNum_PRIVATE_APP) {}
+    EFlagsModule() : SinglePortModule("eflags", meshtastic_PortNum_PRIVATE_APP), OSThread("eflagsModule") {}
+
+    enum FlagState {
+        FLAG_NONE,
+        GREEN_FLAG,
+        YELLOW_FLAG,
+        BLUE_FLAG,
+        WHITE_FLAG,
+        BLACK_FLAG,
+        BLACK_FLAG_ALL,
+        MEATBALL_FLAG,
+        SURFACE_FLAG,
+        RED_FLAG,
+        CHECKERED_FLAG,
+        WAVED_YELLOW,
+        DOUBLE_YELLOW,
+        WAVED_WHITE,
+        WHITE_AND_YELLOW,
+        OPTION
+    };
 
   protected:
-    /** For reply module we do all of our processing in the (normally optional)
-     * want_replies handling
-     */
-    virtual meshtastic_MeshPacket *allocReply() override;
+    virtual ProcessMessage handleReceived(const meshtastic_MeshPacket &mp) override;
+    // virtual meshtastic_MeshPacket *allocReply() override;
+    virtual int32_t runOnce() override;
+
+  private:
+    void sendFlagCommand(NodeNum dest, uint8_t cmd, uint16_t car_num = 0);
+    void setFlagState(uint8_t state, uint16_t car_num);
+    bool firstTime = true;
 };
+
+extern EFlagsModule *eflagsModule;
