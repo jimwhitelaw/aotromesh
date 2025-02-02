8fd6f8440d4fae919034bc33042b4f3f4ab0959b
Jim Whitelaw <jimwhitelaw@gmail.com>
2025-02-02 16:35:21 -0600

    removed unneccesary steps since i2c bus is already initialized.

--- src/modules/EFlagsModule/oledHelper.cpp
+++ src/modules/EFlagsModule/oledHelper.cpp
@@ -1,11 +1,10 @@
 #include "oledHelper.h"
-const uint16_t TRANS_TEMP = 6464;
 // Create display for OLED
 SSD1306 display(0x3c, OLED_SDA, OLED_SCL);
 
 void setupOLEDDisplay()
 {
-    Serial.println("Starting OLED display...");
+    // LOG_INFO("Starting OLED display...");
     // Configure OLED by setting the OLED Reset HIGH, LOW, and then back HIGH
     // pinMode(OLED_RST, OUTPUT);
     // digitalWrite(OLED_RST, HIGH);
@@ -18,7 +17,7 @@ void setupOLEDDisplay()
     delay(200);
     display.clear();
     display.display();
-    Serial.println("OLED init complete.");
+    // Serial.println("OLED init complete.");
 }
 
 void displayData(String text)
